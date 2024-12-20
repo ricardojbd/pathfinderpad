0669fc523f0e745384981310fa1b3deb1c34b330
RicardoJBarrios <ricardojbarrios@gmail.com>
2024-12-20 08:42:39 +0000

    feat(host): change app selector

--- apps/pfdhost/src/app/app.component.ts
+++ apps/pfdhost/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   imports: [RouterModule],
-  selector: 'pfd-root',
+  selector: 'pfd-host',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'pfdhost';
