6fac61bc0baa8e30665956894f9ee58361208f11
RicardoJBarrios <ricardojbarrios@gmail.com>
2024-12-20 08:43:55 +0000

    feat(host): add spells remote

--- apps/pfdhost/src/app/app.routes.ts
+++ apps/pfdhost/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'spells',
+    loadChildren: () =>
+      loadRemoteModule('pfdspells', './Routes').then((m) => m.appRoutes),
+  },
+];
